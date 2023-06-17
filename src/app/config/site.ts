13a615c65a810d365a36034458bc99ac1a9f9035
niawjunior <niawkung@gmail.com>
2023-06-18 00:21:40 +0700

    initial project :tada:

---
+++ src/app/config/site.ts
@@ -0,0 +1,11 @@
+import { SiteConfig } from "../types/index";
+
+export const siteConfig: SiteConfig = {
+  name: "Next.js Template",
+  description: "website description",
+  url: "https://github.com/riad-azz/",
+  ogImageUrl: "/og.png",
+  links: {
+    github: "https://github.com/riad-azz",
+  },
+};
---
+++ src/app/types/index.d.ts
@@ -0,0 +1,9 @@
+export type SiteConfig = {
+  name: string;
+  description: string;
+  url: string;
+  ogImageUrl: string;
+  links: {
+    github: string;
+  };
+};
