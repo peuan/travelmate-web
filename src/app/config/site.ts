df94bd0b476092f0b2c7e2ee4684f03a754b704a
niawjunior <niawkung@gmail.com>
2023-06-18 11:11:09 +0700

    refactor(site.ts): update site url

--- src/app/config/site.ts
+++ src/app/config/site.ts
@@ -3,9 +3,9 @@ import { SiteConfig } from "../types/index";
 export const siteConfig: SiteConfig = {
   name: "Next.js Template",
   description: "website description",
-  url: "https://github.com/riad-azz/",
+  url: "https://github.com/peuan/travelmate-web",
   ogImageUrl: "/og.png",
   links: {
-    github: "https://github.com/riad-azz",
+    github: "https://github.com/peuan/travelmate-web",
   },
 };
