6c7178932c02b91fbdc7f2e451778b28a94fd97d
niawjunior <niawkung@gmail.com>
2023-06-19 11:13:30 +0700

    ci(workflows): add github action workflow to build playwright report

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? "/travelmate-web/" : "",
 };
 
 module.exports = nextConfig;
