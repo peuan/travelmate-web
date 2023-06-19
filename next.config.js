3de38885da46a84468a52e1cb140d72cb7c21e31
Thanachon <thanachonfirst@hotmail.com>
2023-06-19 12:13:11 +0700

    ci(next.config.js): add output option for deployment on GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   assetPrefix: isProd ? "/travelmate-web/" : "",
 };
